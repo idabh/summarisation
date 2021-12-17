52f3f1b3a5ffbb6e1b435d831ce2592740155c2a
idabh <idabanghansen@gmail.com>
2021-12-17 11:04:07 +0100

    no biggy

--- Output_results/inspect_results.py
+++ Output_results/inspect_results.py
@@ -10,7 +10,10 @@ rou = np.load("/work/summarisation/Output_results/mt5_rouge-50k.npy", allow_pick
 rou
 
 #-----MT5 50K NROWS WITH 10 EPOCHS (run 16/12):
+import numpy as np
 met = np.load('mt5_50k_ep10_16_12_2021.npy', allow_pickle=True) #metrics
 res = np.load('results_MT5_50k_ep10_16_12_2021.npy', allow_pickle=True) #results
 rou = np.load('rouge_MT5_50k_ep10_16_12_2021.npy', allow_pickle=True) #rouge_output
 #rouge: 0.54787443, 0.28235127, 0.35684455
+res[:5]
+
